8a971c33f6c1d49fd2df3a1a44e710e85b9743d3
Lakshya <lakshh113@gmail.com>
2025-04-29 12:42:21 +0000

    create new psbt using builder:

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use anyhow::Context;
 use bdk_electrum::BdkElectrumClient;
 use bdk_wallet::Wallet;
-use bdk_wallet::rusqlite;
+use bdk_wallet::{
+    TxBuilder, TxOrdering,
+    bitcoin::{Address, Amount, Network, PublicKey, CompressedPublicKey},
+    rusqlite,
+};
+use std::str::FromStr;
 fn main() -> anyhow::Result<()> {
     let mut db = rusqlite::Connection::open("wallet.db")?;
     let new_wallet = Wallet::load()
@@ -9,13 +14,35 @@ fn main() -> anyhow::Result<()> {
         .expect("failed to load wallet");
 
     let mut wallet = new_wallet.expect("Wallet should be initialized");
-    {
-        let mut txs = wallet.transactions();
-        let first_tx = txs.next().unwrap();
-        let send_and_received = wallet.sent_and_received(&first_tx.tx_node);
-        println!("send_and_received: {:?}", send_and_received);
-    }
     println!("balance is {}", wallet.balance());
+    let pubkey = CompressedPublicKey::from_str(
+        "0202020202020202020202020202020202020202020202020202020202020202",
+    )?;
+
+    // Or if you need to convert from bitcoin::PublicKey to CompressedPublicKey:
+    // let bitcoin_pubkey = PublicKey::from_str("0202020202020202020202020202020202020202020202020202020202020202")?;
+    // let pubkey = CompressedPublicKey::from(bitcoin_pubkey);
+
+    // Fix 2: Pass Network::Testnet by value, not by reference
+    let addr2 = Address::p2wpkh(
+        &pubkey,
+        Network::Testnet, // Remove the & reference
+    );
+    let psbt1 = {
+        let mut builder = wallet.build_tx();
+        builder.add_recipient(addr2.script_pubkey(), Amount::from_sat(50_000));
+        builder.finish()?
+    };
     wallet.persist(&mut db)?;
+
     Ok(())
+    // const ELECTRUM_URL: &str = "ssl://electrum.blockstream.info:60002";
+    // let new_client =
+    //     BdkElectrumClient::new(bdk_electrum::electrum_client::Client::new(ELECTRUM_URL).unwrap());
+    // let request = wallet.start_full_scan();
+    // eprintln!("Starting wallet synchronization...");
+    // let update = new_client.full_scan(request, 5, 90, false)?;
+    // eprintln!("Sync completed. Applying updates...");
+
+    // build tx
 }
