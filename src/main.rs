385eae7be8ba5a20768bbb6b4619e8c1d1cbfb11
Lakshya <lakshh113@gmail.com>
2025-04-29 11:13:02 +0000

    synced with blockchain the revealed addresses

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,16 @@ fn main() -> anyhow::Result<()> {
         .expect("failed to load wallet");
 
     let mut wallet = new_wallet.expect("Wallet should be initialized");
-    for i in 0..100 {
-        let address = wallet.reveal_next_address(bdk_wallet::KeychainKind::External);
-        println!("Address: {}, {:?}", address.to_string(), address.index);
-    }
+    
+    const ELECTRUM_URL: &str = "ssl://electrum.blockstream.info:60002";
+    let new_client =
+        BdkElectrumClient::new(bdk_electrum::electrum_client::Client::new(ELECTRUM_URL).unwrap());
+    let request = wallet.start_full_scan();
+    eprintln!("Starting wallet synchronization...");
+    let update = new_client.full_scan(request, 5, 90, false)?;
+    eprintln!("Sync completed. Applying updates...");
+    wallet.apply_update(update)?;
     wallet.persist(&mut db)?;
     Ok(())
+
 }
